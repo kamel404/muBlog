b3e2f8a355cbd31fa5dd5ea3f7da12a9bd56fa88
ALI ITAWI <116486366+ITAXBOX@users.noreply.github.com>
2025-01-04 12:48:08 +0200

    Post Model

---
+++ app/Models/Post.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'user_id',
+        'image',
+        'title',
+        'location',
+        'body',
+    ];
+
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class)->latest();
+    }
+
+    public function userLikes()
+    {
+        return $this->hasMany(Like::class)->where('user_id', auth()->id());
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
