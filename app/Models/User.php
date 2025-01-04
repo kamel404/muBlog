80285d83e225e8c177a7471e50efe3f3813da558
kamel404 <10121641@mu.edu.lb>
2025-01-04 15:42:00 +0200

    Comment model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,6 +41,16 @@ class User extends Authenticatable
         return $this->belongsTo(Role::class, 'role_id');
     }
 
+    public function posts()
+    {
+        return $this->hasMany(Post::class, 'user_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'user_id');
+    }
+
     public function isAdmin()
     {
         return $this->role_id === 2;
