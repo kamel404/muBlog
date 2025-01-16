e045de67d60565172dd3ef0e2a8ac8a73e5b1cdf
ALI ITAWI <116486366+ITAXBOX@users.noreply.github.com>
2025-01-16 02:14:03 +0200

    Add authentication middleware and kernel configuration

---
+++ app/Http/Kernel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $middleware = [
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+    ];
+
+    protected $middlewareGroups = [
+        'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+
+    protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'role' => \App\Http\Middleware\CheckRole::class,
+    ];
+} 
