dde7010f0a8f8a13930871ac4d1c8fcd9ccccf4c
ALI ITAWI <116486366+ITAXBOX@users.noreply.github.com>
2025-01-21 00:49:45 +0200

    Fix: Fix Seeds

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -11,7 +11,7 @@ class RoleSeeder extends Seeder
 
     public function run(): void
     {
-        Role::create(['name' => 'User']);
-        Role::create(['name' => 'Admin']);
+        Role::create(['name' => 'User']);  // ID: 1
+        Role::create(['name' => 'Admin']); // ID: 2
     }
 }
