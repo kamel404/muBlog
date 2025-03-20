00b05741780ba1689ff664f2ff0553700b3e40b4
Kamel Faour <10121641@mu.edu.lb>
2025-03-20 21:20:34 +0200

    still fixing

--- config/mail.php
+++ config/mail.php
@@ -51,11 +51,7 @@ return [
         ],
 
         'postmark' => [
-            'transport' => 'postmark',
-            // 'message_stream_id' => env('POSTMARK_MESSAGE_STREAM_ID'),
-            // 'client' => [
-            //     'timeout' => 5,
-            // ],
+            'transport' => 'postmark'
         ],
 
         'resend' => [
